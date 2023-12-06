14d428b5bdb81e9cd8ece83c6f46d494312983d3
Sainitin D <s.daverpally@gmail.com>
2023-12-06 13:14:31 -0500

    converted functions into a class

--- src/File_Tree_Generator_SainitinD/main.py
+++ src/File_Tree_Generator_SainitinD/main.py
@@ -37,59 +37,78 @@ import os
 #     print("Tree Ignore NOT Found. Continuing the process...")
 
 
-def create_file_tree(path, last_folder=False):
-    """
-    Creates a file tree when given a path.
-    
-    Args:
-            path (string): A string containing the path of the folder
-            last_folder (boolean): A boolean indicating if the path given is the last folder in the tree.
-                                   Intended to be used by sub-folders so don't touch it unless you have to.
-                                   Used to help formatting. 
-        Returns:
-            out (string): A string containing the file tree of the given path 
-    """
-    # Uncomment for debugging
-    # print(path)
-    
-    out = ""
-    files = os.listdir(path)
-    files = [f for f in files if f not in ignore_folders]  # O(N*K)
-    
-    # Handle edge case
-    if len(files) == 0:
-        return out
-    elif len(files) == 1:
-        if os.path.isfile(files[0]):
-            out += f"└── {files[0]}\n"
-            return out
+class FTree():
+    def __init__(self, input_path, out_path=None, treeignore_path=None):
+        self.input_path = input_path
+        print(f"Received input path: {input_path}")
 
-    for i, file_name in enumerate(files):
-        # Add files/folders into file tree
-        cur_path = os.path.join(path, file_name)
-        if i != len(files)-1:
-            out = out +  f"├── {file_name}\n"
+        if out_path:
+            print(f"Received output path: {out_path}")
         else:
-            out += f"└── {file_name}\n"
+            print(f"Not received output path. Using input path: {input_path}")
+
+        if treeignore_path:
+            print(f"Received tree ignore path: {treeignore_path}")
+        else:
+            print(f"Not received tree ignore path. Using input path: {input_path}")
         
-
-        # if file is a folder, format and add contents of the folder into the file tree
-        if not os.path.isfile(cur_path):
-            is_last_folder = False if i != len(files)-1 else True
-
-            folder_out = create_file_tree(cur_path, is_last_folder)
-            folder_out = folder_out.split("\n")[:-1]
-            for line in folder_out:
-                if is_last_folder:
-                    out = out + "    " + line + "\n"
-                else:
-                    out = out + "│   " + line + "\n"
-    return out
+        self.out_path = out_path if out_path else input_path
+        self.treeignore_path = treeignore_path if treeignore_path else input_path
 
 
-print("Starting the process...")
-out = create_file_tree(input_path)
-with open(os.path.join(output_path, "out.txt"), "w", encoding="utf-8") as f:
-    f.write(out)
-print("Successfully Completed :D")
-print(f"Output stored at... {os.path.join(output_path, 'out.txt')}")
+    def create_file_tree(self, path, last_folder=False):
+        """
+        Creates a file tree when given a path.
+        
+        Args:
+                path (string): A string containing the path of the folder
+                last_folder (boolean): A boolean indicating if the path given is the last folder in the tree.
+                                    Intended to be used by sub-folders so don't touch it unless you have to.
+                                    Used to help formatting. 
+            Returns:
+                out (string): A string containing the file tree of the given path 
+        """
+        # Uncomment for debugging
+        # print(path)
+        
+        out = ""
+        files = os.listdir(path)
+        files = [f for f in files if f not in ignore_folders]  # O(N*K)
+        
+        # Handle edge case
+        if len(files) == 0:
+            return out
+        elif len(files) == 1:
+            if os.path.isfile(files[0]):
+                out += f"└── {files[0]}\n"
+                return out
+
+        for i, file_name in enumerate(files):
+            # Add files/folders into file tree
+            cur_path = os.path.join(path, file_name)
+            if i != len(files)-1:
+                out = out +  f"├── {file_name}\n"
+            else:
+                out += f"└── {file_name}\n"
+            
+
+            # if file is a folder, format and add contents of the folder into the file tree
+            if not os.path.isfile(cur_path):
+                is_last_folder = False if i != len(files)-1 else True
+
+                folder_out = create_file_tree(cur_path, is_last_folder)
+                folder_out = folder_out.split("\n")[:-1]
+                for line in folder_out:
+                    if is_last_folder:
+                        out = out + "    " + line + "\n"
+                    else:
+                        out = out + "│   " + line + "\n"
+        
+        print("Starting the process...")
+        out = create_file_tree(input_path)
+        with open(os.path.join(output_path, "out.txt"), "w", encoding="utf-8") as f:
+            f.write(out)
+        print("Successfully Completed :D")
+        print(f"Output stored at... {os.path.join(output_path, 'out.txt')}")
+        
+        return out
